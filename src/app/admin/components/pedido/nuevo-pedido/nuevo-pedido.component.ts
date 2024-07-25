228949b4a55d71aa4dd56b4f7ea02fe2c7662e6f
cristian <cchura.cpc@gmail.com>
2024-07-25 19:55:23 -0400

    nuevo pedido

--- src/app/admin/components/pedido/nuevo-pedido/nuevo-pedido.component.ts
+++ src/app/admin/components/pedido/nuevo-pedido/nuevo-pedido.component.ts
@@ -1,5 +1,8 @@
 import { Component, inject } from '@angular/core';
 import { ClienteService } from '../../../../core/services/cliente.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { PedidoService } from '../../../../core/services/pedido.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-nuevo-pedido',
@@ -12,8 +15,19 @@ export class NuevoPedidoComponent {
   q: string = "";
   cliente_seleccionado: any = {}
   visible_cliente: boolean = false
+   formCliente: FormGroup;
 
   clienteService= inject(ClienteService)
+  pedidoService= inject(PedidoService)
+
+  constructor(fb: FormBuilder,){
+    this.formCliente = fb.group({
+      nombre_completo: fb.control('', [Validators.required]),
+      telefono: fb.control(''),
+      direccion: fb.control(''),
+      ci: fb.control(''),
+    });
+  }
 
   quitarCarrito(product: any){
     let index = this.carrito.indexOf(product)
@@ -28,5 +42,36 @@ export class NuevoPedidoComponent {
       }
     )
   }
+
+  guardarCliente(){
+    this.clienteService.guardar(this.formCliente.value).subscribe(res => {
+      this.cliente_seleccionado = res;
+      this.visible_cliente = false;
+
+      Swal.fire({
+        title: "Cliente Registrado!",
+        text: "Continuar!",
+        icon: "success"
+      });
+    })
+  }
+
+  guardarPedido(){
+    let datos = {
+      cliente_id: this.cliente_seleccionado.id,
+      productos: this.carrito
+    }
+    this.pedidoService.guardar(datos).subscribe(res => {
+      this.cliente_seleccionado = {}
+      this.carrito = [];
+
+      Swal.fire({
+        title: "Pedido Registrado!",
+        text: "Continuar!",
+        icon: "success"
+      });
+    })
+  }
+  
 
 }
---
+++ src/app/core/services/pedido.service.ts
@@ -0,0 +1,38 @@
+import { Injectable, inject } from '@angular/core';
+import { environment } from '../../../environments/environment';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PedidoService {
+
+  urlBase = environment.servidor
+  http = inject(HttpClient)
+
+  constructor() { }
+
+  listar(page: number = 1, limit: number = 5, q: string=""){
+    return this.http.get(`${this.urlBase}/pedido?page=${page}&limit=${limit}&q=${q}`);
+  }
+
+  guardar(data: any){
+    return this.http.post(`${this.urlBase}/pedido`, data);
+  }
+
+  mostrar(id: number){
+    return this.http.get(`${this.urlBase}/pedido/${id}`);
+  }
+
+  modificar(id: number, data: any){
+    return this.http.put(`${this.urlBase}/pedido/${id}`, data);
+  }
+
+  eliminar(id: number){
+    return this.http.delete(`${this.urlBase}/pedido/${id}`);
+  }
+
+  actualizarImagen(id:number, formData: any){
+    return this.http.post(`${this.urlBase}/pedido/${id}/actualizar-imagen`, formData);
+  }
+}
