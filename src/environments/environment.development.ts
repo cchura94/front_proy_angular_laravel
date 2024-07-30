aa86cbdaa2468371b52d5f5dba957513d036c424
cristian <cchura.cpc@gmail.com>
2024-07-29 20:53:45 -0400

    servidor url

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
-    // servidor: "http://127.0.0.1:8000/api"
-    servidor: "https://cursolaravelangular.blumbit.net/back/public/api"
+    servidor: "http://127.0.0.1:8000/api"
+    // servidor: "https://cursolaravelangular.blumbit.net/back/public/api"
 
 };
